564d6afb65c6354fe6aba994583f4672231e2176
Calvin Storoschuk <calvinsomething@gmail.com>
2021-03-05 12:56:07 -0500

    connected registeration form fields to django form

--- chess/login/views.py
+++ chess/login/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from .forms import RegisterForm
 
 def login_view(request):
+    if request.method == 'POST':
+        pass
     return render(request, "login/login.html")
 
 def register_view(request):
-    return render(request, "login/register.html")
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+    form = RegisterForm()
+
+    for field in form.fields:
+        if field == 'username': placeholder = 'Username'
+        if field == 'email': placeholder = 'Email'
+        if field == 'password1': placeholder = 'Password'
+        if field == 'password2': placeholder = 'Re-Enter Password'
+        form.fields[field].widget.attrs.update({'class': 'form-control', 'placeholder': placeholder})
+
+    return render(request, "login/register.html", {'form': form})
