51d1384ee01452fe61882f0cd94325521d2d1506
Calvin Storoschuk <calvinsomething@gmail.com>
2021-03-05 09:05:43 -0500

    added bootstrap and set up template dirs

--- chess/chess/urls.py
+++ chess/chess/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from login.views import login_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', login_view),
 ]
