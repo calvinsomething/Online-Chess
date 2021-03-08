1dd19d30ad52b3e92e6cf600cacf977612eca3c6
Calvin Storoschuk <calvinsomething@gmail.com>
2021-03-08 16:09:38 -0500

    added superuser and favicon and added image folder to static

--- chess/login/views.py
+++ chess/login/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from .forms import RegisterForm, LoginForm
 from django.contrib.auth.views import LoginView
 from django.views.generic.edit import CreateView
@@ -13,22 +13,5 @@ class Register_View(CreateView):
     template_name = 'login/register.html'
     form_class = RegisterForm
 
-
-
-
-# def register_view(request):
-    
-#     form = RegisterForm(request.POST or None)
-
-#     if form.is_valid():
-#         #form.save()
-#         return redirect('login')
-
-#     for field in form.fields:
-#         if field == 'username': placeholder = 'Username'
-#         if field == 'email': placeholder = 'Email'
-#         if field == 'password1': placeholder = 'Password'
-#         if field == 'password2': placeholder = 'Re-Enter Password'
-#         form.fields[field].widget.attrs.update({'class': 'form-control', 'placeholder': placeholder})
-
-#     return render(request, "login/register.html", {'form': form})
+    def get_success_url(self):
+        return reverse("login")
