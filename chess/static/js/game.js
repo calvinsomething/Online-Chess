8b9ea7d5dfe9272f82df5a1bde7baac99971e3d5
Calvin Storoschuk <calvinsomething@gmail.com>
2021-03-15 12:15:45 -0400

    added icon change for dragging pieces

--- chess/static/js/game.js
+++ chess/static/js/game.js
@@ -1,11 +1,3 @@
-/*
-
-- setBoard() places pieces in starting positions
-- square on mouse down turns cursor (css) into whatever piece is on the square
-- square on release puts piece on that square if that move is legal
-- if piece is taken (or pawn reaches end), piece on square is replaced and image = square's current piece
-
-*/
 
 function setBoard() {
     takenFrom = undefined;
@@ -43,20 +35,42 @@ function draw(square, piece) {
 function pickUpPiece(square) {
     inHand = board[square.id[0]][square.id[2]];
     takenFrom = square.id;
-    //change cursor icon in CSS
+    var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    if(inHand !== '0')
+        if(width > 1300)
+            document.getElementById("board").style.cursor = `url(static/images/pieces/${inHand}.png) 60 60, default`;
+        else if(width > 800)
+            document.getElementById("board").style.cursor = `url(static/images/pieces/Medium/${inHand}.png) 45 45, default`;
+        else
+            document.getElementById("board").style.cursor = `url(static/images/pieces/Small/${inHand}.png) 30 30, default`;
+
     console.log("Picked up piece from " + square.id);
 }
 
 function placePiece(square) {
     console.log("Placed piece on " + square.id);
-    if(inHand !== '0') {
-        board[square.id[0]][square.id[2]] = inHand;
-        draw(square, inHand);
-    }
+    // if move is legal
+    // modify board
+    // then modify display
     if(takenFrom) {
         board[takenFrom[0]][takenFrom[2]] = '0';
         draw(document.getElementById(takenFrom), '0');
     }
+    if(inHand !== '0') {
+        board[square.id[0]][square.id[2]] = inHand;
+        draw(square, inHand);
+    }
+    
+    document.getElementById("board").style.cursor = "default";
+}
+
+function getWinSize() {
+    
+
+    var height = window.innerHeight
+    || document.documentElement.clientHeight
+    || document.body.clientHeight;
+    return [width, height];
 }
 
 function Square() {
