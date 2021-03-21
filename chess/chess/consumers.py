3bd0773c9da8960aee10b5a8dcbeff4215a3ea1b
Calvin Storoschuk <calvinsomething@gmail.com>
2021-03-21 12:37:30 -0400

    updated consumer class to be asynchronous

--- chess/chess/consumers.py
+++ chess/chess/consumers.py
@@ -1,34 +1,33 @@
 import json
-from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 
-class GameConsumer(WebsocketConsumer):
-    def connect(self):
+class GameConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         self.game_id = self.scope['url_route']['kwargs']['game_id']
         self.game_group_id = 'game_%s' % self.game_id
 
         # Join room group
-        async_to_sync(self.channel_layer.group_add)(
+        await self.channel_layer.group_add(
             self.game_group_id,
             self.channel_name
         )
 
-        self.accept()
+        await self.accept()
 
-    def disconnect(self, close_code):
-        # Leave room group
-        async_to_sync(self.channel_layer.group_discard)(
+    async def disconnect(self, close_code):
+        # Leave game group
+        await self.channel_layer.group_discard(
             self.game_group_id,
             self.channel_name
         )
 
     # Receive message from WebSocket
-    def receive(self, text_data):
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         board = text_data_json['board']
 
         # Send move to game group
-        async_to_sync(self.channel_layer.group_send)(
+        await self.channel_layer.group_send(
             self.game_group_id,
             {
                 'type': 'board_update',
@@ -37,10 +36,10 @@ class GameConsumer(WebsocketConsumer):
         )
 
     # Receive board update from game group
-    def board_update(self, event):
+    async def board_update(self, event):
         board = event['board']
 
         # Send message to WebSocket
-        self.send(text_data=json.dumps({
+        await self.send(text_data=json.dumps({
             'board': board
         }))
